ec8389bd34a23c929faa3e83b8c7678d10764224
Xabier Crespo Álvarez <xabicrespog@gmail.com>
2024-01-10 13:33:05 +0100

    Replicate bug without using sunrise crate

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 use esp_idf_sys::{self as _}; // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
-use sunrise;
 
 fn main() {
     // It is necessary to call this function once. Otherwise some patches to the runtime
     // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
     esp_idf_sys::link_patches();
 
-    sunrise::sunrise_sunset(50.5, 50.5, 2022, 5, 3);
-
-    println!("Hello, world!");
+    // Changing the loop to run 2+ times causes an Illegal instruction (with esp-idf-sys = 0.33.2)
+    for i in 0..2 {
+        let n = (50.1 + (i as f32) * 2. % 100.1) * 7.1;
+        println!("Number {n}");
+    }
 }
